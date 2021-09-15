83af3861c01d414e8a6843bb545e2b91fd9cb5c7
basanovase <79385478+basanovase@users.noreply.github.com>
2021-09-15 21:08:11 +1200

    Updates to create file structure

--- build/lib/planpy/__init__.py
+++ build/lib/planpy/__init__.py
@@ -1,9 +1,13 @@
 
-import weakref
+
 from dateutil import parser
+from os import chdir
+from structures import construction, it
 
 
-class tools:
+
+
+class Tools:
 
 
     """
@@ -20,38 +24,35 @@ class tools:
 
         """
         try:
+
             start_date = parser.parse(start_date)
             self.start_date = start_date
-
+            return self.start_date
         except Exception as E:
 
             print(str(E))
 
-        return self.start_date
+
 
 
     def add_end_date(self, end_date):
-        try:
-            end_date = parser.parse(end_date)
-            self.end_date = add_end_date
-            return self.end_date
+        if self.start_date:
 
-        except Exception as E:
+            try:
 
-            print(str(E))
+                end_date = parser.parse(end_date)
+                self.end_date = end_date
+                return self.end_date
+
+            except Exception as E:
+
+                print(str(E))
+
+        else:
+
+            raise ValueError('You must add a start date prior to an end date.')
 
 
-
-
-    def add_budget(self, budget):
-        try:
-            
-            int(budget) = budget
-            self.budget = budget
-            return self.add_budget
-
-        except:
-            print(str(E))
 
 
 
@@ -64,32 +65,42 @@ class tools:
         return self.business_owner
 
 
-class risk(tools):
-    """ Main risk class of library.
+class Risk(Tools):
+    """
+    Main risk class of library.
 
     """
-    def __init__(self, name, project, progress):
+    risks = []
+
+    def __init__(self, name: str, project: str, progress: float):
         self.name = name
         self.project = project
         self.progress = progress
+        self.__class__.risks.append(self)
 
 
 
-class task(tools):
-    """Main task class of library.
+class Task(Tools):
+    """
+
+    Main task class of library.
 
             Inititlise a new project:
                 Methods can then be added off task
 
     """
-    def __init__(self, name, project, progress):
+    tasks = []
+
+    def __init__(self, name, project, progress, fte):
         self.name = name
         self.project = project
         self.progress = progress
+        self.__class__.tasks.append(self)
+        self.fte = fte
 
 
 
-class project(tools):
+class Project(Tools):
 
     """
     Main project class of library.
@@ -100,10 +111,73 @@ class project(tools):
     """
     projects = []
 
-    def __init__(self,project_name):
+    def __init__(self,project_name, project_type="it"):
         self.project_name = project_name
-        self.__class__.projects.append(weakref.proxy(self))
+        self.__class__.projects.append(self)
+
+
 
     def __str__(self):
         for instance in self.projects:
             return instance.name
+
+
+    def create_directory(self, os_dir):
+            os.chdir(os_dir)
+            os.mkdir(project_name)
+            if project_type == "construction":
+
+                for folder in construction.construction:
+                    os.mkdir(folder)
+            else:
+
+                for folder in it.it:
+                    os.mkdir(folder)
+
+
+
+
+
+class Budget:
+
+    def __init__(self, project, total):
+        self.project = project
+        self.total = total
+
+
+    def add_expense():
+        pass
+
+    def add_gst():
+        pass
+
+    def quarterly_projection():
+        pass
+
+    def monthly_projection():
+        pass
+
+    def cost_category():
+        pass
+
+
+class Reporting:
+
+    def __init__(self, project):
+        self.project = project
+
+
+    def month_summary():
+        pass
+
+    def key_risks():
+        pass
+
+    def key_person_dependencies():
+        pass
+
+    def overdue_tasks():
+        pass
+
+    def overdue_risks():
+        pass
